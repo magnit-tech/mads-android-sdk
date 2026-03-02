4a2dbb801d03142f01faba47c8133bd89c6371d9
Kirill Merionkov <k.merionkov@gmail.com>
2026-03-02 15:11:22 +0300

    Update to 0.0.1 release

--- app/src/main/kotlin/ru/tander/mads/demo/MadsDemoApplication.kt
+++ app/src/main/kotlin/ru/tander/mads/demo/MadsDemoApplication.kt
@@ -1,25 +1,18 @@
 package ru.tander.mads.demo
 
 import android.app.Application
-import android.content.Intent
-import androidx.core.net.toUri
 import ru.tander.mads.Mads
+import ru.tander.mads.MadsSdk
 
 class MadsDemoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Mads.init(this, ::openLink)
+        Mads.init(this)
         Mads.applyDefaultConfig()
     }
 
-    private fun openLink(link: String) {
-        Intent(Intent.ACTION_VIEW, link.toUri())
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            .let(::startActivity)
-    }
-
-    private fun Mads.applyDefaultConfig() {
+    private fun MadsSdk.applyDefaultConfig() {
         userId = MadsSdkDefaults.USER_ID
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,3 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
-
-tasks.register<Copy>("installAndroidSdk") {
-
-    val sdkArtifact = file(properties["madsSdk.zip"] ?: "mads.zip")
-    val sdkMavenLocal = file("maven/mads")
-
-    from(zipTree(sdkArtifact))
-    into(sdkMavenLocal)
-
-    inputs.file(sdkArtifact)
-    outputs.dir(sdkMavenLocal)
-}
